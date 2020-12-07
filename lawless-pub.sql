1a075393f1701f8a7203ad0ecdb808cf98d4705f
Faiz Sulistyawan <faiz.sulistyawan@gmail.com>
2020-12-07 11:36:23 +0700

    first commit

--- lawless-pub.sql
+++ lawless-pub.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 13, 2020 at 09:55 AM
+-- Generation Time: Dec 07, 2020 at 05:35 AM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.4.3
 
@@ -32,7 +32,7 @@ CREATE TABLE `histori` (
   `total_harga` int(255) NOT NULL,
   `tgl_pemesanan` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   `kasir` text NOT NULL,
-  `tagihan` int(255) NOT NULL,
+  `tagihan` varchar(255) NOT NULL,
   `id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -41,8 +41,14 @@ CREATE TABLE `histori` (
 --
 
 INSERT INTO `histori` (`total_harga`, `tgl_pemesanan`, `kasir`, `tagihan`, `id`) VALUES
-(50000, '2020-08-02 20:53:59', 'duhdeksusahnya', 3, 13),
-(60000, '2020-08-02 20:56:12', 'duhdeksusahnya', 4, 14);
+(67100, '2020-11-10 08:10:36', 'anya', '1604995834522', 36),
+(769560, '2020-11-10 08:10:44', 'anya', '1604995843464', 37),
+(49500, '2020-11-15 18:45:30', 'anya', '1605465926899', 38),
+(373120, '2020-11-15 19:08:41', 'anya', '1605467200520', 39),
+(153120, '2020-11-15 19:10:16', 'anya', '1605467413485', 40),
+(16500, '2020-11-15 19:11:00', 'kambing', '1605467458977', 41),
+(1135860, '2020-11-15 19:11:31', 'kambing', '1605467489503', 42),
+(96470, '2020-11-30 13:58:38', 'hahaha', '1606744708006', 43);
 
 -- --------------------------------------------------------
 
@@ -79,7 +85,8 @@ CREATE TABLE `level` (
 --
 
 INSERT INTO `level` (`id`, `level`) VALUES
-(1, 'admin');
+(1, 'admin'),
+(2, 'cashier');
 
 -- --------------------------------------------------------
 
@@ -89,7 +96,7 @@ INSERT INTO `level` (`id`, `level`) VALUES
 
 CREATE TABLE `pemesanan` (
   `id_produk` int(11) NOT NULL,
-  `tagihan` int(11) NOT NULL,
+  `tagihan` varchar(255) NOT NULL,
   `kuantitas` int(255) NOT NULL,
   `id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -99,10 +106,17 @@ CREATE TABLE `pemesanan` (
 --
 
 INSERT INTO `pemesanan` (`id_produk`, `tagihan`, `kuantitas`, `id`) VALUES
-(9, 3, 1, 17),
-(15, 3, 1, 18),
-(15, 4, 1, 19),
-(8, 4, 1, 20);
+(9, '1604995834522', 2, 54),
+(10, '1604995834522', 1, 55),
+(38, '1604995843464', 3, 56),
+(10, '1605465926899', 3, 57),
+(39, '1605467200520', 1, 58),
+(40, '1605467413485', 1, 59),
+(10, '1605467458977', 1, 60),
+(39, '1605467489503', 3, 61),
+(10, '1605467489503', 1, 62),
+(10, '1606744708006', 3, 63),
+(38, '1606744708006', 1, 64);
 
 -- --------------------------------------------------------
 
@@ -124,9 +138,11 @@ CREATE TABLE `produk` (
 --
 
 INSERT INTO `produk` (`id`, `nama_produk`, `harga_produk`, `id_kategori`, `added_at`, `gambar_produk`) VALUES
-(8, 'cappucino', 23000, 2, '2020-08-01 13:12:19', 'https://elated-meitner-9eee48.netlify.app/assets/firdaus-roslan-pN769u0KHNA-unsplash.png'),
-(9, 'espresso', 23000, 1, '2020-08-01 13:13:01', 'https://elated-meitner-9eee48.netlify.app/assets/jeremy-ricketts-6ZnhM-xBpos-unsplash.png'),
-(10, 'kopi gula', 15000, 1, '2020-08-12 17:11:53', 'https://elated-meitner-9eee48.netlify.app/assets/bear.png');
+(9, 'DOGLESS dog', 61000, 2, '2020-12-07 04:18:53', 'https://craftlog.com/m/i/6196559=s1280=h960.webp'),
+(10, 'dogless', 20900, 2, '2020-11-26 06:29:36', 'https://craftlog.com/m/i/6196559=s1280=h960.webp'),
+(38, 'red velvet', 25000, 2, '2020-11-24 17:20:57', 'https://craftlog.com/m/i/6196559=s1280=h960.webp'),
+(39, 'chicken katsu', 25000, 1, '2020-11-24 17:20:59', 'https://craftlog.com/m/i/6196559=s1280=h960.webp'),
+(40, 'black forest', 13920, 1, '2020-11-24 17:21:00', 'https://craftlog.com/m/i/6196559=s1280=h960.webp');
 
 -- --------------------------------------------------------
 
@@ -136,6 +152,7 @@ INSERT INTO `produk` (`id`, `nama_produk`, `harga_produk`, `id_kategori`, `added
 
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
   `username` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `level_id` int(11) NOT NULL
@@ -145,10 +162,12 @@ CREATE TABLE `users` (
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `username`, `password`, `level_id`) VALUES
-(1, 'lawless', '$2b$10$iX8VQKkvJ.n5j8PRatBI9e/2qv4Y0JDQemHRwwL1HcF55Z6JR1c6S', 1),
-(2, 'lawless-garage', '$2b$10$JlWBwEsY51NQ9mF07JzV.eOrUmJ5F/qopjQ7wbyvMnXaoAYilhJ0q', 1),
-(4, 'lawless-kitchen', '$2b$10$gLo7dnZOUf/vvHusfM3jueQnsxJ9bHZ0qwlazCdyZDHigv8CahhAC', 1);
+INSERT INTO `users` (`id`, `name`, `username`, `password`, `level_id`) VALUES
+(16, 'kambing', 'lawless', '$2b$10$9P0A.dvEC49qcfzxTGAiA.tRhohMC1qBXLuqy5hwICPueXQ1tQVWO', 1),
+(17, 'anya', 'bukananya', '$2b$10$a33ZAlexTcWDPbgoZew5UOBNKCpgt2Nc/PldWtNZ75hyk491F5byC', 2),
+(20, 'paiz', 'maspaiz', '$2b$10$JMmObFyKs/VfHGhZRhtZM.KRwzJTJvykwoxquPd.1dPVHDYslJaWC', 2),
+(26, 'bukansiapa', 'siapaya', '$2b$10$CiX1BpTwyPTdKltAOCDum.Som8RrMh1RrOnCaphg/oQ4/FqCdiAGm', 2),
+(29, 'hahaha', 'bukansiapasiapa', '$2b$10$PiMqsW7jLWsyIzu3/A1wKOlbFgaQ0lMEzEK/U4GzZtmuQr3QiT0ai', 2);
 
 --
 -- Indexes for dumped tables
@@ -198,7 +217,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `histori`
 --
 ALTER TABLE `histori`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=44;
 
 --
 -- AUTO_INCREMENT for table `kategori`
@@ -210,25 +229,25 @@ ALTER TABLE `kategori`
 -- AUTO_INCREMENT for table `level`
 --
 ALTER TABLE `level`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `pemesanan`
 --
 ALTER TABLE `pemesanan`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=65;
 
 --
 -- AUTO_INCREMENT for table `produk`
 --
 ALTER TABLE `produk`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=114;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
